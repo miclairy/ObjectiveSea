8d6f5562c2a2deb385e26e94ecfbadebd63ce126
Michael Trotter <mjt169@uclive.ac.nz>
2017-03-06 14:33:26 +1300

    #313: Print out starter list

--- src/main/java/seng302/Display.java
+++ src/main/java/seng302/Display.java
@@ -1,7 +1,20 @@
 package seng302;
 
+import java.util.ArrayList;
+
 /**
  * Created by mjt169 on 6/03/17.
+ * Class to manage the output display.
+ * For now this will be simple text-based output to terminal
  */
+
 public class Display {
+
+    public static void printStartersList(ArrayList<String> starters) {
+        System.out.println("Boats in this race:");
+        for (String boat : starters) {
+            System.out.println(boat);
+        }
+    }
+
 }
--- src/main/java/seng302/RaceVisionFileReader.java
+++ src/main/java/seng302/RaceVisionFileReader.java
@@ -1,7 +1,37 @@
 package seng302;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 /**
  * Created by mjt169 on 6/03/17.
+ * Collection of methods for reading in data from files
  */
+
 public class RaceVisionFileReader {
+
+    private static final String DATA_PATH = "data/";
+    private static final String STARTERS_FILE = "starters.txt";
+
+    public static ArrayList<String> importStarters(){
+        ArrayList<String> starters = new ArrayList<String>();
+        String filePath = DATA_PATH + STARTERS_FILE;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+            String line = br.readLine();
+            while (line != null){
+                starters.add(line);
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.err.printf("Starter file could not be found at %s\n", filePath);
+        } catch (IOException e) {
+            System.err.printf("Error reading start file. Check it is in the correct format.");
+        }
+
+        return starters;
+    }
 }
