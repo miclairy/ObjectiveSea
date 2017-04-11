955fbc9c254229b175d7a993c48f66793be8f236
sjh298 <uc188cav>
2017-04-11 14:36:28 +1200

    #pair[cba62, sjh298] #story[29.1] added to mock stream class to make a server and open a socket and send hello

--- src/main/java/seng302/controllers/MockStream.java
+++ src/main/java/seng302/controllers/MockStream.java
@@ -1,11 +1,41 @@
 package seng302.controllers;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+
 public class MockStream implements Runnable {
 
 
     @Override
     public void run() {
+        BufferedReader inFromUser =
+                new BufferedReader(new InputStreamReader(System.in));
+        DatagramSocket clientSocket = null;
+        try {
+            clientSocket = new DatagramSocket();
+            InetAddress IPAddress = InetAddress.getByName("localhost");
+            byte[] sendData = new byte[1024];
+            byte[] receiveData = new byte[1024];
+            String sentence = "Heeelllloooo";
+            sendData = sentence.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+            clientSocket.send(sendPacket);
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+            String modifiedSentence = new String(receivePacket.getData());
+            System.out.println("FROM SERVER:" + modifiedSentence);
+            clientSocket.close();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
