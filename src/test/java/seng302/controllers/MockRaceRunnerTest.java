87f277177c6c0ff8a8fe09614d6beba06ede97e1
Michael Trotter <mjt169@uclive.ac.nz>
2017-05-11 15:11:45 +1200

    Moved update location tests to MockRaceRunnerTest
    - Moved missed test from RaceTest to MockRaceRunnerTest
    
    #story[902]

--- src/test/java/seng302/controllers/MockRaceRunnerTest.java
+++ src/test/java/seng302/controllers/MockRaceRunnerTest.java
@@ -245,4 +245,48 @@ public class MockRaceRunnerTest {
         assertEquals(51.585, boat.getCurrentLat(), DELTA);
         assertEquals(30.15, boat.getCurrentLon(), DELTA);
     }
+
+    @Test
+    public void checkFinishPlacings(){
+        Course course = new Course();
+
+        Mark startLine1 = new Mark(0, "Start Line 1", new Coordinate(51.55, 30.11));
+        Mark startLine2 = new Mark(1, "Start Line 2", new Coordinate(51.60, 30.16));
+        RaceLine start = new RaceLine(1, "Start Line", startLine1, startLine2);
+
+        start.setMarkAsStart();
+        course.setStartLine(start);
+
+        Mark finishLine1 = new Mark(2, "Finish Line 1", new Coordinate(51.56, 30.12));
+        Mark finishLine2 = new Mark(3, "Finish Line 2", new Coordinate(51.61, 30.18));
+        RaceLine finish = new RaceLine(2, "Finish", finishLine1, finishLine2);
+        finish.setMarkAsFinish();
+
+        course.addNewCompoundMark(start);
+        course.addNewCompoundMark(finish);
+
+        course.addMarkInOrder(1);
+        course.addMarkInOrder(2);
+        course.setStartLine(start);
+        ArrayList<Boat> boats = new ArrayList<>();
+
+        Boat testBoat1 = new Boat(0, "Boat 1","1", 10);
+        Boat testBoat2 = new Boat(1, "Boat 2", "2",15);
+        testBoat1.setLastRoundedMarkIndex(0);
+        testBoat2.setLastRoundedMarkIndex(0);
+
+        boats.add(testBoat1);
+        boats.add(testBoat2);
+
+        Race race = new Race("Mock Race", course, boats);
+        MockRaceRunner runner = new MockRaceRunner(race);
+
+        runner.updateLocation(2, course, testBoat1);
+        runner.updateLocation(2, course, testBoat2);
+
+        List<Boat> places = new ArrayList<>();
+        places.add(testBoat1);
+        places.add(testBoat2);
+        assertEquals(places, race.getRaceOrder());
+    }
 }
