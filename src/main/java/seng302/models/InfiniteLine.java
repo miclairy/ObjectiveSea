92a1c2522cf0b994b2aad60a41b92fd3fd225c5f
Ray Chen <atc60@uclive.ac.nz>
2017-05-19 18:16:55 +1200

    Added docstrings and unit tests for InfinteLine methods #story[906]

--- src/main/java/seng302/models/InfiniteLine.java
+++ src/main/java/seng302/models/InfiniteLine.java
@@ -2,18 +2,23 @@ package seng302.models;
 
 /**
  * Defines a line with equation ax + by + c = 0
- *
+ * (Equation used instead of y = mx + c to avoid dealing with infinite m when line is vertical)
  */
 public class InfiniteLine {
     private static final double EPS = 1e-9;
     private double a, b, c;
 
-    public InfiniteLine(double a, double b, double c) {
+    private InfiniteLine(double a, double b, double c) {
         this.a = a;
         this.b = b;
         this.c = c;
     }
 
+    /**
+     * Creates an infinite line that crosses through the two given points
+     * @param point1 The first point which defines the line
+     * @param point2 The second point which defines the line
+     */
     InfiniteLine(Coordinate point1, Coordinate point2){
         if(Math.abs(point1.getLon() - point2.getLon()) < EPS){
             a = 1.0;
@@ -26,24 +31,35 @@ public class InfiniteLine {
         }
     }
 
+    /**
+     * Finds the coordinate on this line that is closest to the given point
+     * @param point The point to find the closest point for
+     * @return The closest point to the given point
+     */
     public Coordinate closestPoint(Coordinate point){
         Coordinate closest;
-        if(this.isVerticalLine()){
+        if(this.isVertical()){
             closest = new Coordinate(point.getLat(), -c);
-        } else if(this.isHorizontalLine()){
+        } else if(this.isHorizontal()){
             closest = new Coordinate(-c, point.getLon());
         } else{
-            InfiniteLine perpendicular = this.pointSlopeToLine(point, 1 / a);
+            InfiniteLine perpendicular = pointSlopeToLine(point, 1 / a);
             closest = InfiniteLine.intersectionPoint(this, perpendicular);
         }
         return closest;
     }
 
-    private static Coordinate intersectionPoint(InfiniteLine infiniteLine1, InfiniteLine infiniteLine2) {
+    /**
+     * Finds the intersection point between two lines. Assumes the two lines are not the same or parallel
+     * @param infiniteLine1 The first line
+     * @param infiniteLine2 The second line
+     * @return The point that lies on both line 1 and line 2
+     */
+    public static Coordinate intersectionPoint(InfiniteLine infiniteLine1, InfiniteLine infiniteLine2) {
         double lon = (infiniteLine1.getB() * infiniteLine2.getC() - infiniteLine2.getB() * infiniteLine1.getC()) /
                      (infiniteLine1.getA() * infiniteLine2.getB() - infiniteLine2.getA() * infiniteLine1.getB());
         double lat;
-        if(!infiniteLine1.isVerticalLine()){
+        if(!infiniteLine1.isVertical()){
             lat = -(infiniteLine1.getA() * lon + infiniteLine1.getC());
         } else{
             lat = -(infiniteLine2.getA() * lon + infiniteLine2.getC());
@@ -51,18 +67,24 @@ public class InfiniteLine {
         return new Coordinate(lat, lon);
     }
 
-    private InfiniteLine pointSlopeToLine(Coordinate point, double slope) {
+    /**
+     * Given a point and slope, finds the line with the given slope that crosses through the given point
+     * @param point The point the line will pass through
+     * @param slope The slope of the line
+     * @return The line created by the given values of point and slope
+     */
+    public static InfiniteLine pointSlopeToLine(Coordinate point, double slope) {
         double a = -slope;
         double b = 1;
         double c = -((a * point.getLon()) + (b * point.getLat()));
         return new InfiniteLine(a, b ,c);
     }
 
-    private boolean isVerticalLine() {
+    public boolean isVertical() {
         return Math.abs(b) < EPS;
     }
 
-    private boolean isHorizontalLine() {
+    public boolean isHorizontal() {
         return Math.abs(a) < EPS;
     }
 
