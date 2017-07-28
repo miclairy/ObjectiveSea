38ae8ebb2d1b67dd87795a83c8797b87dddc275f
Chris Donovan <cjd137@uclive.ac.nz>
2017-07-28 17:45:33 +1200

    Removed unessassary comments.

--- src/main/java/seng302/utilities/GameSounds.java
+++ src/main/java/seng302/utilities/GameSounds.java
@@ -7,12 +7,6 @@ import javax.sound.sampled.*;
  * Created by Chris on 28/07/2017.
  */
 
-/**
- * A simple Java sound file example (i.e., Java code to play a sound file).
- * AudioStream and AudioPlayer code comes from a javaworld.com example.
- * @author alvin alexander, devdaily.com.
- */
-
 public class GameSounds {
 
     public void startLobbyMusic() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
