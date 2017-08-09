5d8f69208f6a57ed72f3979a2373d47d1f9eb616
Chris Donovan <cjd137@uclive.ac.nz>
2017-08-10 11:53:10 +1200

    Changing names of sound files for easier importing #story[1135]

--- src/main/java/seng302/utilities/GameSounds.java
+++ src/main/java/seng302/utilities/GameSounds.java
@@ -9,13 +9,60 @@ import javax.sound.sampled.*;
 
 public class GameSounds {
 
-    public void startLobbyMusic() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
-        Clip clip = AudioSystem.getClip();
-        AudioInputStream inputStream = AudioSystem.getAudioInputStream(DisplayUtils.class.getResourceAsStream("/musicFiles/StringBeat.wav"));
+
+    String chris_boat_time = "/musicFiles/ChrisVoiceOvers/boat_time.wav";
+    String chris_collided = "/musicFiles/ChrisVoiceOvers/collided.wav";
+    String chris_holly_heck = "/musicFiles/ChrisVoiceOvers/holly_heck.wav";
+    String chris_list_this_race = "/musicFiles/ChrisVoiceOvers/lost_this_race.wav";
+    String chris_mateies = "/musicFiles/ChrisVoiceOvers/maties.wav";
+    String chris_ready_race = "/musicFiles/ChrisVoiceOvers/ready_race.wav";
+    String chris_rubber_dingy = "/musicFiles/ChrisVoiceOvers/rubber_dingy.wav";
+    String chris_sail_ebration = "/musicFiles/ChrisVoiceOvers/sail_ebration.wav";
+    String chris_scream = "/musicFiles/ChrisVoiceOvers/scream.wav";
+
+
+
+
+
+    private Clip clip;
+    private AudioInputStream inputStream;
+
+    public void startMenuMusic() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+        clip = AudioSystem.getClip();
+        inputStream = AudioSystem.getAudioInputStream(DisplayUtils.class.getResourceAsStream("/musicFiles/gameSounds/MainMenuMusic.wav"));
         clip.open(inputStream);
         clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void startTutorialMusic() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+        clip = AudioSystem.getClip();
+        inputStream = AudioSystem.getAudioInputStream(DisplayUtils.class.getResourceAsStream("/musicFiles/gameMusic/TutorialMusic.wav"));
+        clip.open(inputStream);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void startOceanWaves() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+        clip = AudioSystem.getClip();
+        inputStream = AudioSystem.getAudioInputStream(DisplayUtils.class.getResourceAsStream("/musicFiles/gameSounds/CrispOceanWaves.wav"));
+        clip.open(inputStream);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void flockSeagulls() throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+        clip = AudioSystem.getClip();
+        inputStream = AudioSystem.getAudioInputStream(DisplayUtils.class.getResourceAsStream("/musicFiles/gameSounds/FlockOfSeagulls.wav"));
+        clip.open(inputStream);
+    }
+
+
+
+    public void startMusic() {
         clip.start();
     }
 
+    public void stopMusic() throws LineUnavailableException {
+        clip.stop();
+        clip.setFramePosition(0);
+    }
 }
 
