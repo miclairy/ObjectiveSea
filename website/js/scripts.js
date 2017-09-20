8b0f7a359df79387f5de18003376dcfd427b398d
Samuel Hooker <samjhooker@gmail.com>
2017-09-20 16:40:39 +1200

    created a UI for controls
    #implement #story[1321]

--- website/js/scripts.js
+++ website/js/scripts.js
@@ -1,3 +1,21 @@
+
+
+// $(document).ready(function(){
+//     $( "#codeEntryForm" ).on( "c", loadControls());
+//
+// });
+
+
+function loadControls(){
+
+  console.log("hello");
+  $("#codeForm").toggle(2000);
+    $("#controls").toggle(2000);
+
+
+}
+
+
 let socket = new WebSocket("ws://132.181.13.96:2828", "checkingLifeWorks");
 
 socket.onopen = function (event) {
