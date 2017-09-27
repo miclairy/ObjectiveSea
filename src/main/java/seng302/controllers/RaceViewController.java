f9e9ad4c04a4e5ad44c67c7044abacebd19acc52
Devin Davies <dda40@uclive.ac.nz>
2017-09-28 09:22:56 +1300

    fixed issue where HUD would not display on a joiners visualiser
    #story[1363]

--- src/main/java/seng302/controllers/RaceViewController.java
+++ src/main/java/seng302/controllers/RaceViewController.java
@@ -424,6 +424,7 @@ public class RaceViewController extends AnimationTimer implements Observer {
             if(boatDisplay.getBoat().getId() == Main.getClient().getClientID()){
                 currentUserBoatDisplay = boatDisplay;
                 scoreBoardController.highlightUserBoat();
+                controller.addUserBoatHUD();
             }
         }
     }
